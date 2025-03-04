61b77dcaf4d3627d12698b5f7f7cf367d4c0ca92
Mat95rix7 <ndjam71@gmail.com>
2025-03-04 01:07:20 +0100

    seo

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,10 +1,32 @@
 // Fichier: api/sitemap.js
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, query, where, limit } from 'firebase/firestore';
-// import sallesCinema from '../../src/data/cinemas.json';
-import { Fetching } from '../../src/services/tmdb';
+import sallesCinema from './cinemas.json';
 
+
+  const Fetching = (endpoint)=>{
+  const [data,setData] = useState([])
+  const [loading,setLoading] = useState(false)
+
+  const fetchData = async()=>{
+      try {
+          setLoading(true)
+          const response = await axios.get(`${endpoint}?include_adult=false&language=fr-FR`)
+          setLoading(false)
+          setData(response.data.results)
+      } catch (error) {
+          console.log('error',error)
+     }
+  }
+
+  useEffect(()=>{
+      fetchData()
+  },[endpoint])
+
+  return { data , loading}
+}
 // Configuration Firebase
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
